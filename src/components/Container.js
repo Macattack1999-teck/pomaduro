ce19000c9660cb404a5f6566626a1bc1538016af
Michael Cortez <CortezDevelopement@gmail.com>
2021-02-02 18:57:21 -0700

    created new folders and basic skeletons for components Ill be using in this project

---
+++ src/components/Container.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div style={{ height: "100%", minHeight: "100vh", width: "100%", background: "blue" }}>
+      Hello 
+    </div>
+  )
+}
