28f153f887089edaddcdad0c5ed637c3ccc689f6
Michael Cortez <CortezDevelopement@gmail.com>
2021-02-02 20:17:49 -0700

    now changing break length in Session component ith onclick handlers

--- src/components/Break.js
+++ src/components/Break.js
@@ -3,8 +3,21 @@ import EverythingContext from '../contexts/EverythingContext'
 
 export default () => {
   const {
-    breakLength
+    breakLength,
+    setBreakLength
   } = useContext(EverythingContext);
+
+  const handleUpdatingbreakLength = (btn) => {
+    if (btn === '-') {
+      if (breakLength > 1) {
+        setBreakLength(breakLength - 1)
+      }
+    } else {
+      if (breakLength < 60) {
+        setBreakLength(breakLength + 1)
+      }
+    }
+  }
 
   return (
     <div>
@@ -17,7 +30,7 @@ export default () => {
         justifyContent: "space-evenly",
         alignItems: "center"
       }}>
-        <div>
+        <div onClick={() => handleUpdatingbreakLength('-')}>
           -
         </div>
 
@@ -25,7 +38,7 @@ export default () => {
           {breakLength}
         </div>
 
-        <div>
+        <div onClick={() => handleUpdatingbreakLength('+')}>
           +
         </div>
       </div>
