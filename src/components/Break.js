cbf834746a9e1527679cdc50e7ac6f67179388a4
Michael Cortez <CortezDevelopement@gmail.com>
2021-02-02 20:02:45 -0700

    using context state in Break component and session component

--- src/components/Break.js
+++ src/components/Break.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import EverythingContext from '../contexts/EverythingContext'
 
 export default () => {
+  const {
+    breakLength
+  } = useContext(EverythingContext);
+
   return (
     <div>
-      break
+      <div>
+        break Length (mins)
+      </div>
+
+      <div style={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        alignItems: "center"
+      }}>
+        <div>
+          Up
+        </div>
+
+        <div>
+          {breakLength}
+        </div>
+
+        <div>
+          Down
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Session.js
+++ src/components/Session.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import EverythingContext from '../contexts/EverythingContext'
 
 export default () => {
+  const {
+    sessionLength
+  } = useContext(EverythingContext);
+
   return (
     <div>
-      session
+      <div>
+        Session Length (mins)
+      </div>
+
+      <div style={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        alignItems: "center"
+      }}>
+        <div>
+          Up
+        </div>
+
+        <div>
+          {sessionLength}
+        </div>
+
+        <div>
+          Down
+        </div>
+      </div>
     </div>
   )
 }
