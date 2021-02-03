454fa77823f2c27fe5cf6c0758b5d370b387b20e
Michael Cortez <CortezDevelopement@gmail.com>
2021-02-02 19:03:07 -0700

    added basic skeleton to Container component along with basic styles

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,8 +2,39 @@ import React from 'react'
 
 export default () => {
   return (
-    <div style={{ height: "100%", minHeight: "100vh", width: "100%", background: "blue" }}>
-      Hello 
+    <div 
+      style={{
+        height: "100%",
+        minHeight: "100vh",
+        width: "100%",
+        background: "#185687",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-evenly"
+      }}
+    >
+      <div>
+        Pomaduro
+      </div>
+
+      <div>
+        some component
+      </div>
+
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+      }}>
+        Designed and Coded by:
+
+        <span style={{
+          fontSize: "14px",
+          paddingTop: "5px"
+        }}>
+          Michael Cortez
+        </span>
+      </div>
     </div>
   )
 }
